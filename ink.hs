50924cd18cfb30ea30de6a3b6394bc1cd6a9689e
Daniel Collins <mail@dcollins.info>
2014-07-09 13:32:35 +1000

    removed the interpretive stuff

--- ink.hs
+++ ink.hs
@@ -19,30 +19,6 @@ data CpsExpression = A Uatom
                    | B Katom
                    | C Call
 
-data Stack a = EmptyStack
-             | StackEntry a (Stack a)
-
-type State = (CpsExpression, Stack CpsExpression, Stack CpsExpression)
-
-push :: a -> Stack a -> Stack a
-push value stack = StackEntry value stack 
-
-pop :: Stack a -> (Stack a, Maybe a)
-pop EmptyStack = (EmptyStack, Nothing)
-pop (StackEntry v s) = (s, Just v)
-
-evalUatom :: Uatom -> CpsExpression
-evalUatom atom = A atom
-
-evalKatom :: Katom -> CpsExpression
-evalKatom atom = B atom
-
-evalCall :: Call -> CpsExpression
-evalCall call = C call
-
-step :: State -> State
-step s = s
-
 main :: IO ()
 main = putStrLn "hi"
 
