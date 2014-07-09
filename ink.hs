0c6390c7deb0d9d7adf697fcb6a590d7be7098ec
Daniel Collins <mail@dcollins.info>
2014-07-09 10:09:43 +1000

    added data type for non CPS expression

--- ink.hs
+++ ink.hs
@@ -1,24 +1,28 @@
-type Uvariable = String
+type Variable = String
+
+data Expression = Reference Variable 
+                | Lambda Variable Expression
+                | Combination Expression Expression 
 
 type Kvariable = String
 
-data Uatom = Procedure Uvariable Kvariable Call
-           | Ureference Uvariable
+data Uatom = Procedure Variable Kvariable Call
+           | Ureference Variable
 
-data Katom = Continuation Uvariable Call
-           | Kreference Uvariable
+data Katom = Continuation Variable Call
+           | Kreference Variable
 
 data Call = Application Uatom Uatom Katom
           | Invocation Katom Uatom
 
-data Expression = A Uatom
-                | B Katom
-                | C Call
+data CpsExpression = A Uatom
+                   | B Katom
+                   | C Call
 
 data Stack a = EmptyStack
              | StackEntry a (Stack a)
 
-type State = (Expression, Stack Expression, Stack Expression)
+type State = (CpsExpression, Stack CpsExpression, Stack CpsExpression)
 
 push :: a -> Stack a -> Stack a
 push value stack = StackEntry value stack 
@@ -27,13 +31,13 @@ pop :: Stack a -> (Stack a, Maybe a)
 pop EmptyStack = (EmptyStack, Nothing)
 pop (StackEntry v s) = (s, Just v)
 
-evalUatom :: Uatom -> Expression
+evalUatom :: Uatom -> CpsExpression
 evalUatom atom = A atom
 
-evalKatom :: Katom -> Expression
+evalKatom :: Katom -> CpsExpression
 evalKatom atom = B atom
 
-evalCall :: Call -> Expression
+evalCall :: Call -> CpsExpression
 evalCall call = C call
 
 step :: State -> State
