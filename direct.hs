a7c947132e3bb997246b41e4cd929e7e3c66baf5
Daniel Collins <mail@dcollins.info>
2014-07-12 23:38:39 +1000

    instance of show for direct form language

--- direct.hs
+++ direct.hs
@@ -6,4 +6,9 @@ data Expression = Reference Variable
                 | Lambda Variable Expression
                 | Combination Expression Expression
 
+instance Show Expression where
+  show (Reference v) = v
+  show (Lambda v b) = "(LAMBDA (" ++ v ++ ") " ++ (show b) ++ ")"
+  show (Combination a b) = "(" ++ (show a) ++ " " ++
+    (show b) ++ ")" 
 
