8c1cc62b253a61d57bd55f46ffb2bc1e92fb3362
Daniel Collins <mail@dcollins.info>
2014-07-12 10:42:55 +1000

    add instances for show manually

--- ink.hs
+++ ink.hs
@@ -8,16 +8,29 @@ type Kvariable = String
 
 data Uatom = Procedure Variable Kvariable Call
            | Ureference Variable
-  deriving (Show)
+
+instance Show Uatom where
+  show (Ureference r) = r
+  show (Procedure p k b) = "(LAMBDA (" ++ p ++ " " ++ k ++
+     ") " ++ show b ++ ")"
 
 data Katom = Continuation Variable Call
            | Kreference Variable
            | Absorb
-  deriving (Show)
+
+instance Show Katom where
+  show (Continuation p b) = "(CONTINUATION " ++ show p ++
+    " " ++ show b ++ ")"
+  show (Kreference v) = v
+  show Absorb = "ABSORB"
 
 data Call = Application Uatom Uatom Katom
           | Invocation Katom Uatom
-  deriving (Show)
+
+instance Show Call where
+  show (Application p a k) = "(" ++ show p ++ " " ++ show a ++
+     " " ++ show k ++ ")"
+  show (Invocation k a) = "(" ++ show k ++ " " ++ show a ++ ")"
 
 cpsTransform :: Expression -> Katom -> Call
 cpsTransform (Reference r) k = Invocation k $ Ureference r
@@ -27,5 +40,5 @@ cpsTransform (Lambda p b) k = Invocation k $ Procedure p
 cpsTransform (Combination a b) k = cpsTransform  a $ Continuation "k" $ cpsTransform b k
 
 main :: IO ()
-main = print $ cpsTransform (Reference "a") $ Absorb
+main = print $ cpsTransform (Reference "a") Absorb
 
